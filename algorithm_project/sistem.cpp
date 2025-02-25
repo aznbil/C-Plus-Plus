21d539983ec98e3506b5356a8739e9e4ae860d4a
aziz <aziznabil9859@gmail.com>
2025-02-25 10:25:17 +0700

    Membuat menu Create data

--- algorithm_project/sistem.cpp
+++ algorithm_project/sistem.cpp
@@ -8,12 +8,19 @@ struct Buku{
   int tahun_terbit;
 };
 
+struct Pekerjaan {
+  string role;
+};
+
 struct Anggota{
-  int id;
-  char nama[50];
+  int id; 
+  Pekerjaan role;
+  char nama[50]; //struct dalam struct ada mahasiswa, dosen, pegawai jangan langsung nama. nanti dipanggil id nya 
   string alamat;
   char telepon[15];
-};
+  };
+  
+
 
 struct Peminjaman{
   int idPeminjam;
@@ -22,3 +29,114 @@ struct Peminjaman{
   string tanggal_pinjam;
   string tanggal_kembali;
 };
+
+void create_buku();
+void create_anggota();
+void create_peminjaman();
+
+int main(){
+  while(true){
+  create_buku();
+  create_anggota();
+  create_peminjaman();
+  }
+  return 0;
+}
+
+void create_buku(){
+  Buku buku[100];
+  int n;
+  
+  cout << "Berapa data yang ingin di input? ";
+  cin >> n;
+  if(n > 100){
+    cout << "Input melebihi batas" << endl;
+    return;
+  }
+  if(n < 1){
+    cout << "Input tidak valid" << endl;
+    return;
+  }
+  
+  system("cls");
+  for (int i = 0; i < n; i++){
+    cout << "Id buku\t\t: ";
+    cin >> buku[i].id;
+    cin.ignore();
+    cout << "Judul buku\t: ";
+    getline(cin, buku[i].judul);
+    cout << "Penulis buku\t: ";
+    getline(cin, buku[i].penulis);
+    cout << "Tahun terbit\t: ";
+    cin >> buku[i].tahun_terbit;
+
+    cout << endl;
+  }
+}
+
+void create_anggota(){
+  Anggota anggota[100];
+  int n;
+  
+  cout << "Berapa data yang ingin di input? ";
+  cin >> n;
+  if(n > 100){
+    cout << "Input melebihi batas" << endl;
+    return;
+  }
+  if(n < 1){
+    cout << "Input tidak valid" << endl;
+    return;
+  }
+  
+  system("cls");
+  for (int i = 0; i < n; i++){
+  cout << "Id anggota : ";
+  cin >> anggota[i].id;
+  cin.ignore();
+  cout << "Nama anggota : ";
+  cin >> anggota[i].nama;
+  cout << "Pekerjaan anggota : ";
+  getline(cin, anggota[i].role.role);
+  cout << "Alamat anggota : ";
+  getline(cin, anggota[i].alamat);
+  cout << "Telepon anggota : ";
+  cin >> anggota[i].telepon;
+}
+
+}
+
+void create_peminjaman(){
+  Peminjaman pinjaman[100];
+  int n;
+
+  cout << "Berapa data yang ingin di input? ";
+  cin >> n;
+  if(n > 100){
+    cout << "Input melebihi batas" << endl;
+    return;
+  }
+  if(n < 1){
+    cout << "Input tidak valid" << endl;
+    return;
+  }
+
+  for (int i = 0; i < n; i++){  
+  cout << "Id peminjam : ";
+  cin >> pinjaman[i].idPeminjam;
+  cin.ignore();
+  cout << "Id buku : ";
+  cin >> pinjaman[i].idBuku;
+  cin.ignore();
+  cout << "Id anggota : ";
+  cin >> pinjaman[i].idAnggota;
+  cin.ignore();
+  cout << "Tanggal pinjam : ";
+  getline(cin, pinjaman[i].tanggal_pinjam);
+  cout << "Tanggal kembali : ";
+  getline(cin, pinjaman[i].tanggal_kembali);
+  }
+
+}
+
+
