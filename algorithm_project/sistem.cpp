eb649a7804ecb092c558e87cb5ed61d64c4f71fc
Bintang Shada <bintangshada45@gmail.com>
2025-03-04 03:20:00 +0700

    feat: read data recursive

--- algorithm_project/sistem.cpp
+++ algorithm_project/sistem.cpp
@@ -42,9 +42,9 @@ void create_anggota();
 void create_peminjaman();
 
 // ini bagian untuk membaca data
-void read_buku();
-void read_anggota();
-void read_peminjaman();
+void read_buku(Buku buku[], int n, int index = 0);
+void read_anggota(Anggota anggota[], int n, int index = 0);
+void read_peminjaman(Peminjaman pinjaman[], int n, int index = 0);
 
 // ini bagian untuk memperbarui data
 void update_buku();
@@ -56,6 +56,11 @@ void delete_buku();
 void delete_anggota();
 void delete_peminjaman();
 
+Buku daftarBuku[100];
+Anggota daftarAnggota[100];
+Peminjaman daftarPeminjaman[100];
+int jumlahBuku = 0, jumlahAnggota = 0, jumlahPeminjaman = 0;
+
 int main(){
   menu();
   return 0;
@@ -63,32 +68,34 @@ int main(){
 
 void menu(){
   int choose;
+  do{
+    cout << "Welcome" << endl;
+    cout << "1. Create " << endl;
+    cout << "2. Read " << endl;
+    cout << "3. Update " << endl;
+    cout << "4. Delete " << endl;
+    cout << "5. Exit " << endl;
+    cout << "Pilih menu : ";
+    cin >> choose;
 
-  cout << "Welcome" << endl;
-  cout << "1. Create " << endl;
-  cout << "2. Read " << endl;
-  cout << "3. Update " << endl;
-  cout << "4. Delete " << endl;
-  cout << "5. Exit " << endl;
-  cout << "Pilih menu : ";
-  cin >> choose;
-
-  switch(choose){
-    case 1:
-      menu_Create();
-      break;
-    case 2:
-      break;
-    case 3:
-      break;
-    case 4:
-      break;
-    case 5:
-      break;
-    default:
-      cout << "Input tidak valid" << endl;
-      break;
-  }
+    switch(choose){
+      case 1:
+        menu_Create();
+        break;
+      case 2:
+        menu_Read();
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+      case 5:
+        break;
+      default:
+        cout << "Input tidak valid" << endl;
+        break;
+    }
+  }while(choose > 5);
 }
 
 void menu_Create(){
@@ -213,4 +220,71 @@ void create_peminjaman(){
 
 }
 
+void read_buku(Buku buku[], int n, int index){
+  if (index == n) return;
+  cout << "Id Buku: " << buku[index].id << endl;
+  cout << "Judul: " << buku[index].judul << endl;
+  cout << "Penulis: " << buku[index].penulis << endl;
+  cout << "Tahun Terbit: " << buku[index].tahun_terbit << endl;
+  cout << "----------------------\n";
+  read_buku(buku, n, index + 1);
+}
 
+void read_anggota(Anggota anggota[], int n, int index){
+  if (index == n) return;
+  cout << "Id Anggota: " << anggota[index].id << endl;
+  cout << "Nama: " << anggota[index].nama << endl;
+  cout << "Pekerjaan: " << anggota[index].role.role << endl;
+  cout << "Alamat: " << anggota[index].alamat << endl;
+  cout << "Telepon: " << anggota[index].telepon << endl;
+  cout << "----------------------\n";
+  read_anggota(anggota, n, index + 1);
+}
+
+void read_peminjaman(Peminjaman pinjaman[], int n, int index){
+  if (index == n) return;
+  cout << "Id Peminjam: " << pinjaman[index].idPeminjam << endl;
+  cout << "Id Buku: " << pinjaman[index].idBuku << endl;
+  cout << "Id Anggota: " << pinjaman[index].idAnggota << endl;
+  cout << "Tanggal Pinjam: " << pinjaman[index].tanggal_pinjam << endl;
+  cout << "Tanggal Kembali: " << pinjaman[index].tanggal_kembali << endl;
+  cout << "----------------------\n";
+  read_peminjaman(pinjaman, n, index + 1);
+}
+
+void menu_Read(){
+  int choose;
+  cout << "\nMenu Read" << endl;
+  cout << "1. Buku" << endl;
+  cout << "2. Anggota" << endl;
+  cout << "3. Peminjaman" << endl;
+  cout << "Pilih menu: ";
+  cin >> choose;
+  
+  switch (choose){
+      case 1:
+          if (jumlahBuku == 0) {
+              cout << "Tidak ada data buku." << endl;
+          } else {
+              read_buku(daftarBuku, jumlahBuku);
+          }
+          break;
+      case 2:
+          if (jumlahAnggota == 0) {
+              cout << "Tidak ada data anggota." << endl;
+          } else {
+              read_anggota(daftarAnggota, jumlahAnggota);
+          }
+          break;
+      case 3:
+          if (jumlahPeminjaman == 0) {
+              cout << "Tidak ada data peminjaman." << endl;
+          } else {
+              read_peminjaman(daftarPeminjaman, jumlahPeminjaman);
+          }
+          break;
+      default:
+          cout << "Input tidak valid" << endl;
+          break;
+  }
+}
